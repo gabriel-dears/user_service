6aa58e82a541ac92269854070d8bfb93db756876
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-09-09 11:09:22 -0300

    feat: Adding mTLS for gRPC communication

--- src/main/java/com/hospital_app/user_service/infra/config/grpc/GrpcSecurityConfig.java
+++ src/main/java/com/hospital_app/user_service/infra/config/grpc/GrpcSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.hospital_app.user_service.infra.config.grpc;
 
 import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
+import net.devh.boot.grpc.server.security.check.AccessPredicate;
+import net.devh.boot.grpc.server.security.check.GrpcSecurityMetadataSource;
+import net.devh.boot.grpc.server.security.check.ManualGrpcSecurityMetadataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,4 +14,13 @@ public class GrpcSecurityConfig {
     public GrpcAuthenticationReader grpcAuthenticationReader() {
         return (call, headers) -> null;
     }
+
+
+    @Bean
+    GrpcSecurityMetadataSource grpcSecurityMetadataSource() {
+        ManualGrpcSecurityMetadataSource source = new ManualGrpcSecurityMetadataSource();
+        source.setDefault(AccessPredicate.permitAll());
+        return source;
+    }
+
 }
