27f62b55a335ccca88d04e2d65df62a780b9d95d
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-09-09 17:32:09 -0300

    feat: Adding README.md file - hospital_app

--- src/main/java/com/hospital_app/user_service/infra/config/DataInitializer.java
+++ src/main/java/com/hospital_app/user_service/infra/config/DataInitializer.java
@@ -4,6 +4,7 @@ import com.hospital_app.user_service.application.port.in.user.CreateUserUseCase;
 import com.hospital_app.user_service.application.port.out.user.CustomUserRepository;
 import com.hospital_app.user_service.domain.model.Role;
 import com.hospital_app.user_service.domain.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,19 +13,26 @@ import org.springframework.context.annotation.Configuration;
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner initAdminUser(CreateUserUseCase createUserUseCase, CustomUserRepository customUserRepository) {
+    CommandLineRunner initAdminUser(
+            @Value("${INITIAL_ADMIN_NAME}") String initialAdminUserName,
+            @Value("${INITIAL_ADMIN_USERNAME}") String initialAdminUserUsername,
+            @Value("${INITIAL_ADMIN_EMAIL}") String initialAdminUserEmail,
+            @Value("${INITIAL_ADMIN_CPF}") String initialAdminUserCpf,
+            @Value("${INITIAL_ADMIN_PASSWORD}") String initialAdminUserPassword,
+            CreateUserUseCase createUserUseCase,
+            CustomUserRepository customUserRepository) {
         return args -> {
 
-            if( customUserRepository.existsByUsername("admin") ) {
+            if(customUserRepository.existsByUsername(initialAdminUserUsername)) {
                 return;
             }
 
             User user = new User();
-            user.setName("Admin");
-            user.setUsername("admin");
-            user.setEmail("admin@email.com");
-            user.setCpf("063.086.090-48");
-            user.setPasswordHash("admin");
+            user.setName(initialAdminUserName);
+            user.setUsername(initialAdminUserUsername);
+            user.setEmail(initialAdminUserEmail);
+            user.setCpf(initialAdminUserCpf);
+            user.setPasswordHash(initialAdminUserPassword);
             user.setRole(Role.ADMIN);
             createUserUseCase.execute(user);
         };
